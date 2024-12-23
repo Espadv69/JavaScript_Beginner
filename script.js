e48219183d5d9523d29a5774130b0ff5a80a411f
espadv69 <aitorjob69@gmail.com>
2024-12-23 18:29:12 +0100

    Update: - Add new variable declarations and explanations in script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 console.log('Hello, world!'); // console.log is a function that prints to the console.
 
-let name = 'Aitor'; // let is keyword that declares a variable.
+let name = 'Aitor'; // let is keyword that declares a variable. let is a variable, so it can be changed.
+const lastName = 'Díaz'; // const is a keyword that declares a constant. const is a constant, so it can't be changed.
+var isDeveloper = true; // var is a keyword that declares a global variable. var is a variable, so it can be changed.
