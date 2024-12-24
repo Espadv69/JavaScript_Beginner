c40cd77b7cba1cc2a8291781f0c113a36272c75a
espadv69 <aitorjob69@gmail.com>
2024-12-24 17:25:18 +0100

    Update: - Add operator exercises and comparison examples

---
+++ Exercises/operator-exercises.js
@@ -0,0 +1,19 @@
+// 1. Create a variable for each arithmetic operator.
+
+// 2. Create a variable for each type of assignment operation, using the variables used for arithmetic operations.
+
+// 3. Print 5 true comparisons using different comparison operators.
+
+// 4. Print 5 false comparisons using different comparison operators.
+
+// 5. Use AND operator
+
+// 6. Use OR operator
+
+// 7. Use both AND and OR operators in the same statement.
+
+// 8. Add some negation to the previous statement.
+
+// 9. Use the ternary operator.
+
+// 10. Combine the arithmetic operators with the comparison operators and logical operators.
