e1b4b212b9a54c0c2a55dafee231178749844da8
espadv69 <aitorjob69@gmail.com>
2024-12-24 15:05:12 +0100

    Update: - Add beginner exercises for JavaScript primitive data types and comments

---
+++ beginner-exercises.js
@@ -0,0 +1,19 @@
+// 1. Write a single-line comment.
+
+// 2. Write a multi-line comment.
+
+// 3. Declare variables with values associated with all primitive data types.
+
+// 4. Print per console the value of each variable.
+
+// 5. Print per console the type of each variable.
+
+// 6. Next, modify the values of the variables with others of the same type.
+
+// 7. Next, modify the values of the variables with others of different types.
+
+// 8. Declare constants with values associated with all primitive data types.
+
+// 9. Next, try to modify the value of a constant.
+
+// 10. Comment the lines that produce any kind of error when executed.
