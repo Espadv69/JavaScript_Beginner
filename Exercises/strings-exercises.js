d74772884c030316a9bcc0ddccaa88c1573b4cf0
espadv69 <aitorjob69@gmail.com>
2024-12-25 12:07:02 +0100

    Update: - Exercises statement

---
+++ Exercises/strings-exercises.js
@@ -0,0 +1,19 @@
+// 1. Concatenate 2 strings.
+
+// 2. Show the length of a string.
+
+// 3. Show the first and the last character of a string.
+
+// 4. Convert a string to uppercase and lowercase.
+
+// 5. Create a multiline string.
+
+// 6. Interpolate the value of a variable into a string.
+
+// 7. Replace all whitespace in a string with hyphens.
+
+// 8. Check if a string contains a specific word.
+
+// 9. Check if two strings are equal.
+
+// 10. Check if two strings have the same length.
