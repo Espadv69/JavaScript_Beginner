7c6af2f24185be4939e153b6ab9ca3aa004e92d0
espadv69 <aitorjob69@gmail.com>
2024-12-25 18:55:10 +0100

    Update: - Exercises statement

---
+++ Exercises/conditionals-exercises.js
@@ -0,0 +1,21 @@
+// 1. Print your name to the console if a variable takes its value.
+
+// 2. Print a message to the console if the username and password match the established ones.
+
+// 3. Check if a number is positive, negative, or zero, and print a message.
+
+// 4. Check if a person can vote (18 or older) and indicate how many years are left if they can't.
+
+// 5. Use the ternary operatot to assign the value 'Adult' or 'Minor (underage)' to a variable depending on the age.
+
+// 6. Display which season of the year we are in depending on the value of a variable 'month'.
+
+// 7. Display the number of days in a month based on the variable from the previous exercise.
+
+
+/*----- SWITCH EXERCISES -----*/
+// 8. Use a switch statement to print a different greeting message depending on the language.
+
+// 9. Use a switch statement to redo exercise 6.
+
+// 10. Use a switch statement to redo exercise 7.
