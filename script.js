ed784c4dddb5479ad0b6ce456195d8f0948ba1f5
espadv69 <aitorjob69@gmail.com>
2024-12-25 22:31:28 +0100

    Update: - Add array manipulation examples in script.js

--- script.js
+++ script.js
@@ -123,6 +123,15 @@ console.log(names) // Expected output: ['Julian', 'Roberto', 'Pepe']
 
 names.unshift('Aitor', 'Lolito') // unshift adds one or more elements to the beginning of an array.
 console.log(names) // Expected output: ['Aitor', 'Lolito', 'Julian', 'Roberto', 'Pepe']
+
+const Names = names.slice(0, 4) // The slice() method in JavaScript is used to extract a portion of an array or a string. From 0 to 4.
+console.log(Names)  // Expected output: ['Aitor', 'Lolito', 'Julian', 'Roberto']
+
+const lastNames = ['Diaz', 'Garcia', 'Lorca', 'Dominguez', 'Perez', 'Parra']
+console.log(lastNames) // Expected output: ['Diaz', 'Garcia', 'Lorca', 'Dominguez', 'Perez', 'Parra']
+
+lastNames.splice(2, 2) // The splice() method is a built-in JavaScript method that allows you to modify an array by removing or replacing existing elements and/or adding new elements.
+console.log(lastNames) // Expected output: ['Diaz', 'Garcia', 'Perez', 'Parra']
 // ---------------------
 
 
