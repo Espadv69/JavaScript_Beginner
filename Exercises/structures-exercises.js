a12e12f77dd651134ac6395ffc6170197e4f4ffa
espadv69 <aitorjob69@gmail.com>
2024-12-26 09:57:09 +0100

    Update: - Exercises statement.

---
+++ Exercises/structures-exercises.js
@@ -0,0 +1,19 @@
+// 1. Create an array that stores 5 animals.
+
+// 2. Add two more, one at the beginning and one at the end.
+
+// 3. Remove the one in the third position.
+
+// 4. Create a set that stores 5 books.
+
+// 5. Add two more, one of them repeated.
+
+// 6. Remove one specific one of your choice.
+
+// 7. Create a map that associates the month number with its name.
+
+// 8. Check if month number 5 exists in the map and print its value.
+
+// 9. Add to the map a key with an array that stores the summer months.
+
+// 10. Create an array, transform it into a set, and store it in a map.
