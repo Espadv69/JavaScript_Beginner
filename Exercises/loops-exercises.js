669445f65edcde6f02d22c2e2d29ea097ece2bfd
espadv69 <aitorjob69@gmail.com>
2024-12-26 13:03:58 +0100

    Update: - Exercises statement

---
+++ Exercises/loops-exercises.js
@@ -0,0 +1,20 @@
+// NOTE: Explore different loop syntaxes to solve the exercises.
+// 1. Create a loop that prints the numbers from 1 to 20.
+
+// 2. Create a loop that prints the numbers from 1 to 100 and displays the result.
+
+// 3. Create a loop that prints all the even numbers between 1 and 50
+
+// 4. Create an array of names and use a loop to print each name to the console.
+
+// 5. Create a llop that counts the number of vowels in a string.
+
+// 6. Create an array of numbers, use a loop to multiply all the numbers, and display the product.
+
+// 7. Create a loop that prints the multiplication table of 5.
+
+// 8. Use a loop to reverse a string.
+
+// 9. Use a loop to generate the first 10 numbers of the Fibonacci sequence.
+
+// 10. Create an array of numbers and use a loop to create a new array with numbers greater than 10.
