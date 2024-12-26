b210d92049ab6655b71fc02f26043695f4003998
espadv69 <aitorjob69@gmail.com>
2024-12-26 18:17:25 +0100

    Add examples of variable declarations in JavaScript, demonstrating let, const, and var usage

---
+++ js-basics/variables.js
@@ -0,0 +1,12 @@
+console.log('Hello, world!') // console.log is a function that prints to the console.
+
+let realName = 'Aitor' // let is keyword that declares a variable. let is a variable, so it can be changed.
+const lastName = 'Díaz' // const is a keyword that declares a constant. const is a constant, so it can't be changed.
+var isDeveloper = true // var is a keyword that declares a global variable. var is a variable, so it can be changed.
+
+console.log(realName) // Prints 'Aitor'.
+console.log(lastName) // Prints 'Díaz'.
+console.log(isDeveloper) // Prints 'true', because isDeveloper is a boolean variable.
+
+realName = 'Pepe' // Changes the value of realName to 'Pepe'. This is possible because realName is a variable.
+console.log(realName) // Now prints 'Pepe'.
