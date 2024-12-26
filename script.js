3e22b5f71e9989e5ba7d79ffcfc867d68418bb8a
espadv69 <aitorjob69@gmail.com>
2024-12-26 12:31:04 +0100

    Update. - Add examples of for-of and do-while loops in script.js

--- script.js
+++ script.js
@@ -404,16 +404,57 @@ for (let i = 0; i < 3; i++) {
 
 const testArrayFor = ['Aitor', 'Pedro', 'Espadv', 'Paco', 'Manolo']
 // This loop iterates over each item in the array and prints its index and value.
-for(let i = 0; i < testArrayFor.length; i++) {
+for (let i = 0; i < testArrayFor.length; i++) {
   console.log(`Item ${i}: ${testArrayFor[i]}`)
 }
+/*---------------------------------------------*/
+
+
+/* The for of loop in JavaScript iterates over the values of an iterable object (like an array or string). */
+// It simplifies accessing each element directly without needing an index.
+let forOfArray = [1, 2, 3, 4]
+let forOfSet = new Set(['Aitor', 'Pepe', 'Roberto', 'Luffy'])
+let forOfMap = new Map([
+  [1, 'Lunes'],
+  [2, 'Martes'],
+  [3, 'Miercoles']
+])
+
+// This loop iterates over each element in 'forOfArray', logging each value to the console.
+for (let value of forOfArray) {
+  console.log(value)
+}
+
+// This loop iterates over each value in 'forOfSet', logging each value to the console.
+for (let value of forOfSet) {
+  console.log(value)
+}
+
+// This loop iterates over each value in 'forOfMap', logging each value to the console.
+for (let value of forOfMap) {
+  console.log(value)
+}
+/*---------------------------------------------*/
+
 
 /* The "while" loop repeats a block of code as long as the specified condition is true. */
 // It checks the condition before each iteration.
 let counter = 0;
+// This loop runs while 'counter' is less than 2, printing its value and incrementing it each time.
 while (counter < 2) {
   console.log(`While ${counter}`)
   counter++
 }
+/*---------------------------------------------*/
 
+
+/* The `do while` loop in JavaScript executes a block of code once and then repeats the loop as long as a specified condition is true. */
+// The condition is evaluated after the code execution.
+counter = 0
+// This loop executes at least once, printing 'counter' and incrementing it, until 'counter' reaches 3.
+do {
+  console.log(counter)
+  counter++
+} while (counter < 3)
+/*---------------------------------------------*/
 // ---------------------
