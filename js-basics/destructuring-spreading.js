484e8c6af452e912f5d383973add29ebd6a63549
espadv69 <aitorjob69@gmail.com>
2024-12-27 23:16:45 +0100

    Update: - Add examples of object property access and array destructuring in JavaScript

---
+++ js-basics/destructuring-spreading.js
@@ -0,0 +1,18 @@
+// How to give a variable the value of an object property in JavaScript.
+let numbers = [1, 2, 3, 4, 5]
+const person = {
+  firstName: 'Aitor',
+  age: 21,
+  isDeveloper: true,
+}
+
+const myName = person.firstName
+console.log(myName) // output: Aitor
+
+const one = numbers[0]
+console.log(one) // output: 1
+
+
+// Destructuring. (Syntax of an array)
+let [first, second, third, fourth, fifth] = numbers
+console.log(first + second) // output: 3
