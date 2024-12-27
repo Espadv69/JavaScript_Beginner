ce41d77fc3efec2dcf7a0807403b0bd31672f7ee
espadv69 <aitorjob69@gmail.com>
2024-12-27 21:37:05 +0100

    Update: - Enhance objects.js with additional examples of object methods and properties

--- js-basics/objects.js
+++ js-basics/objects.js
@@ -30,15 +30,52 @@ console.log(person.isDeveloper) // output: undefined
 person.isDeveloper = true
 console.log(person.isDeveloper) // output: true
 
-// Methods in JavaScript objects.
+// how to add a function as a property.
 let person2 = {
   firstName: 'Pepe',
   lastName: 'Pérez',
   ig: '@pepe69',
   isDeveloper: true,
-  walk: function(bool) { // we can add a function as a property.
-    return bool ? 'I am walking' : 'I am not walking.'
-  }
+  // we can add a function as a property.
+  walk: function(bool) { 
+    return bool ? 'I am walking.' : 'I am not walking.'
+  },
 }
 
 console.log(person2.walk(true)) // output: I am walking.
+
+// How to anidate objects.
+let person3 = {
+  firstName: 'Ramón',
+  lastName: 'García',
+  ig: '@ramoncin',
+  isDeveloper: true,
+  walk: (bool) => bool ? 'I am walking.' : 'I am not walking.',
+  job: {
+    title: 'Frontend Developer',
+    company: 'Vanilla JS',
+    salary: 2000,
+    exp: 10,
+    work: function() {
+      // we can access to the parent object properties, using (this).
+      console.log(`I am working as a ${this.title} in ${this.company}.`)
+    }
+  }
+}
+
+console.log(person3) 
+console.log(person3.job.work(true))
+
+// How to loop through an object in JavaScript.
+for (let key in person3) {
+  console.log(`${key}: ${person3[key]}`)
+}
+
+// Function as a object property.
+function personF(name, age) { // This should be a class. But we will see that later. This is a bad practice.
+  this.name = name
+  this.age = age
+}
+
+let person4 = new personF('Aitor', 21)
+console.log(person4) // output: Aitor
