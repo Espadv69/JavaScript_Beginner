6b16fcfde22e8d4a1523c0e2fde16b5ced7c00ef
espadv69 <aitorjob69@gmail.com>
2024-12-27 22:17:27 +0100

    excercises stament.

---
+++ Exercises/objects-exercises.js
@@ -0,0 +1,19 @@
+// 1. Create an object with 3 properties.
+
+// 2. Access to the object and show the properties in the console.
+
+// 3. Add a new property to the object.
+
+// 4. Delete one of the three properties.
+
+// 5. Add a function and call it.
+
+// 6. Iterate the object's properties.
+
+// 7. Create an anidate object.
+
+// 8. Access to the anidate object and show the properties in the console.
+
+// 9. Check if the two objects are equals.
+
+// 10. Check if two differents properties are equals.
