d2e343f106498cc0e3b4262ac6c787c7522ca2d1
espadv69 <aitorjob69@gmail.com>
2024-12-27 10:25:08 +0100

    Update: - Add functions to count consonants, convert strings to uppercase, check for prime numbers, and find common elements in arrays

--- Exercises/functions-exercises.js
+++ Exercises/functions-exercises.js
@@ -30,8 +30,8 @@ function counterVowels(string) {
   }
   return counter
 }
-
 console.log(counterVowels('Hola, que tal'))
+
 
 function counterConsonants(string) {
   const consonants = 'qwrtypñlkjhgfdszxcvbnmQWRTYPÑLKJHGFDSZXCVBNM'
@@ -45,10 +45,41 @@ function counterConsonants(string) {
   return counter
 }
 console.log(counterConsonants('Hola picha'))
+
+
 // 4. Create a function that receives an array of strings and returns a new one with the strings in uppercase.
+const stringsLowerCase = ['hola', 'coche', 'mapa', 'cabina', 'carrera', 'ponche']
+
+function convert(array) {
+  return array.map(item => item.toUpperCase())
+}
+
+const result = convert(stringsLowerCase)
+console.log(result)
+
+
+/* Without function */
+const stringsLowerCase_2 = ['hola', 'coche', 'mapa', 'cabina', 'carrera', 'ponche', 'marina', 'luffy', 'one piece', 'gato', 'perro']
+const toUpper = stringsLowerCase_2.map(item => item.toUpperCase())
+console.log(toUpper)
+
+
+const largeStrings = ['HOLA', 'COCHE', 'MAPA', 'CABINA', 'CARRERA', 'PONCHE']
+const toLower = largeStrings.map(item => item.toLowerCase())
+console.log(toLower)
 
 
 // 5. Create a function that receives a number and returns true if the number is prime, and false otherwise.
+function isPrime(number) {
+  if (number < 2) return false
+
+  for (let i = 2; i < number; i++) {
+    if (number % i == 0) return false
+  }
+
+  return true
+}
+console.log(isPrime(7))
 
 
 // 6. Create a function that receives two arrays and returns a new one with the common elements between both.
