3ef2d8b77a7e15bebbe2d18ece0066e25b2665a2
espadv69 <aitorjob69@gmail.com>
2024-12-28 10:00:20 +0100

    Update destructuring-spreading.js to enhance examples for array and object copying

--- js-basics/destructuring-spreading.js
+++ js-basics/destructuring-spreading.js
@@ -39,9 +39,15 @@ console.log(compañia) // output: React.js
 let myArray = [...numbers] // Syntax of an array. (This is a copy of the array.)
 console.log(myArray) // output: [1, 2, 3, 4, 5]
 
-let myArray2 = [0, ...numbers, 6, 7, 8] // Syntax of an array. (This is a copy of the array with new values.)
+let myArray2 = [0, ...numbers, 6, 7, 8] // (This is a copy of the array with new values.)
 console.log(myArray2) // output: [0, 1, 2, 3, 4, 5, 6, 7, 8]
 
 // How to combine two arrays.
 let myArray3 = [...myArray, ...myArray2]
 console.log(myArray3) // output: [1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 6, 7, 8]
+
+let myObject = { ...person } // Syntax of an object. (This is a copy of the object.)
+console.log(myObject) // output: {firstName: 'Aitor', age: 21, isDeveloper: true, married: false, job: {title: 'Frontend Developer', company: 'React.js', salary: 2000}}
+
+let myObject2 = { ...person, email: 'name@example.com' } // (This is a copy of the object with new values.)
+console.log(myObject2)
