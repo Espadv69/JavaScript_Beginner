c1c7ae05b3d0bfa69b587c3a27d930a4184700a8
espadv69 <aitorjob69@gmail.com>
2024-12-29 12:45:08 +0100

    Add loop exercises statement with todos for various programming challenges

---
+++ Exercises/loops-exercises-2.js
@@ -0,0 +1,38 @@
+// 1. Print Numbers 1 to 10.
+// Todo: Write a loop that prints nummbers  from 1 to 100. Use for, while, or do while loops.
+
+
+// 2. Sum of array elements.
+// Todo: Create a function that takes an array of numbers as input and returns the sum of all its elements using a loop.
+
+
+// 3. FizzBuzz Chanllenge.
+// Todo: Write a program that prints numbers from 1 to 100. For multiples of 3, print 'Fizz' instead of the number, for multiples of 5, print 'Buzz' instead of the number, and for numbers that are multiples of both, print 'FizzBuzz'.
+
+
+// 4. Reverse a string.
+// Todo: Write a function that takes a string as input and reverses it using a loop.
+
+
+// 5. Factorial Calculator.
+// Todo: Write a function that calculates the factorial of a given number using a loop.
+
+
+// 6. Find the largest number is an array.
+// Todo: Write a function that takes an array of numbers and finds the largest one using a loop.
+
+
+// 7. Check if a number is prime.
+// Todo: Write a function that checks if a given number is prime.
+
+
+// 8. Count vowels in a string.
+// Todo: Create a function that takes a string and counts how many vowels it contains. Use a loop to iterate through the characters.
+
+
+// 9. Generate multiplication table.
+// Todo: Write a program that generates the multiplication table (from 1 to 10) using a loop.
+
+
+// 10. Fibonacci sequence.
+// Todo: Write a function that generates the first numbers in the Fibonacci sequence using a loop.
