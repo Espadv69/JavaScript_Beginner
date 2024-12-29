20be4c1410bffecaebc87bd002e9551d3647c161
espadv69 <aitorjob69@gmail.com>
2024-12-29 12:50:15 +0100

    Update: - First exercise: Add loop implementations for printing numbers from 1 to 100

--- Exercises/loops-exercises-2.js
+++ Exercises/loops-exercises-2.js
@@ -1,5 +1,21 @@
 // 1. Print Numbers 1 to 10.
 // Todo: Write a loop that prints nummbers  from 1 to 100. Use for, while, or do while loops.
+let counter = 0
+while (counter <= 100) {
+  console.log('while: ' + counter)
+  counter++
+}
+
+counter = 0
+do {
+  console.log('do while: ' + counter)
+  counter++
+} while (counter <= 100)
+
+
+for (let i = 0; i <= 100; i++) {
+  console.log('for: ' + i)
+}
 
 
 // 2. Sum of array elements.
